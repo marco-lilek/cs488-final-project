02f791e6d0daf581cb5f9c5fe6bd156ca432ff35
marco-lilek <marco@lilek.ca>
2018-07-06 16:57:35 -0700

    initial commit

---
+++ Project.cpp
@@ -0,0 +1,498 @@
+#include "Project.hpp"
+#include "scene_lua.hpp"
+using namespace std;
+
+#include "cs488-framework/GlErrorCheck.hpp"
+#include "cs488-framework/MathUtils.hpp"
+#include "GeometryNode.hpp"
+#include "JointNode.hpp"
+
+#include <imgui/imgui.h>
+
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/io.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace glm;
+
+static bool show_gui = true;
+
+const size_t CIRCLE_PTS = 48;
+
+//----------------------------------------------------------------------------------------
+// Constructor
+Project::Project(const std::string & luaSceneFile)
+	: m_luaSceneFile(luaSceneFile),
+	  m_positionAttribLocation(0),
+	  m_normalAttribLocation(0),
+	  m_vao_meshData(0),
+	  m_vbo_vertexPositions(0),
+	  m_vbo_vertexNormals(0)
+{
+
+}
+
+//----------------------------------------------------------------------------------------
+// Destructor
+Project::~Project()
+{
+
+}
+
+//----------------------------------------------------------------------------------------
+/*
+ * Called once, at program start.
+ */
+void Project::init()
+{
+	// Set the background colour.
+	glClearColor(0.35, 0.35, 0.35, 1.0);
+
+	createShaderProgram();
+
+	glGenVertexArrays(1, &m_vao_meshData);
+	enableVertexShaderInputSlots();
+
+	processLuaSceneFile(m_luaSceneFile);
+
+	// Load and decode all .obj files at once here.  You may add additional .obj files to
+	// this list in order to support rendering additional mesh types.  All vertex
+	// positions, and normals will be extracted and stored within the MeshConsolidator
+	// class.
+	unique_ptr<MeshConsolidator> meshConsolidator (new MeshConsolidator{
+			getAssetFilePath("cube.obj"),
+			getAssetFilePath("sphere.obj"),
+			getAssetFilePath("suzanne.obj")
+	});
+
+
+	// Acquire the BatchInfoMap from the MeshConsolidator.
+	meshConsolidator->getBatchInfoMap(m_batchInfoMap);
+
+	// Take all vertex data within the MeshConsolidator and upload it to VBOs on the GPU.
+	uploadVertexDataToVbos(*meshConsolidator);
+
+	mapVboDataToVertexShaderInputLocations();
+
+	initPerspectiveMatrix();
+
+	initViewMatrix();
+
+	initLightSources();
+
+
+	// Exiting the current scope calls delete automatically on meshConsolidator freeing
+	// all vertex data resources.  This is fine since we already copied this data to
+	// VBOs on the GPU.  We have no use for storing vertex data on the CPU side beyond
+	// this point.
+}
+
+//----------------------------------------------------------------------------------------
+void Project::processLuaSceneFile(const std::string & filename) {
+	// This version of the code treats the Lua file as an Asset,
+	// so that you'd launch the program with just the filename
+	// of a puppet in the Assets/ directory.
+	// std::string assetFilePath = getAssetFilePath(filename.c_str());
+	// m_rootNode = std::shared_ptr<SceneNode>(import_lua(assetFilePath));
+
+	// This version of the code treats the main program argument
+	// as a straightforward pathname.
+	m_rootNode = std::shared_ptr<SceneNode>(import_lua(filename));
+	if (!m_rootNode) {
+		std::cerr << "Could not open " << filename << std::endl;
+	}
+}
+
+//----------------------------------------------------------------------------------------
+void Project::createShaderProgram()
+{
+	m_shader.generateProgramObject();
+	m_shader.attachVertexShader( getAssetFilePath("VertexShader.vs").c_str() );
+	m_shader.attachFragmentShader( getAssetFilePath("FragmentShader.fs").c_str() );
+	m_shader.link();
+
+}
+
+//----------------------------------------------------------------------------------------
+void Project::enableVertexShaderInputSlots()
+{
+	//-- Enable input slots for m_vao_meshData:
+	{
+		glBindVertexArray(m_vao_meshData);
+
+		// Enable the vertex shader attribute location for "position" when rendering.
+		m_positionAttribLocation = m_shader.getAttribLocation("position");
+		glEnableVertexAttribArray(m_positionAttribLocation);
+
+		// Enable the vertex shader attribute location for "normal" when rendering.
+		m_normalAttribLocation = m_shader.getAttribLocation("normal");
+		glEnableVertexAttribArray(m_normalAttribLocation);
+
+		CHECK_GL_ERRORS;
+	}
+
+
+	// Restore defaults
+	glBindVertexArray(0);
+}
+
+//----------------------------------------------------------------------------------------
+void Project::uploadVertexDataToVbos (
+		const MeshConsolidator & meshConsolidator
+) {
+	// Generate VBO to store all vertex position data
+	{
+		glGenBuffers(1, &m_vbo_vertexPositions);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_vbo_vertexPositions);
+
+		glBufferData(GL_ARRAY_BUFFER, meshConsolidator.getNumVertexPositionBytes(),
+				meshConsolidator.getVertexPositionDataPtr(), GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		CHECK_GL_ERRORS;
+	}
+
+	// Generate VBO to store all vertex normal data
+	{
+		glGenBuffers(1, &m_vbo_vertexNormals);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_vbo_vertexNormals);
+
+		glBufferData(GL_ARRAY_BUFFER, meshConsolidator.getNumVertexNormalBytes(),
+				meshConsolidator.getVertexNormalDataPtr(), GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		CHECK_GL_ERRORS;
+	}
+
+}
+
+//----------------------------------------------------------------------------------------
+void Project::mapVboDataToVertexShaderInputLocations()
+{
+	// Bind VAO in order to record the data mapping.
+	glBindVertexArray(m_vao_meshData);
+
+	// Tell GL how to map data from the vertex buffer "m_vbo_vertexPositions" into the
+	// "position" vertex attribute location for any bound vertex shader program.
+	glBindBuffer(GL_ARRAY_BUFFER, m_vbo_vertexPositions);
+	glVertexAttribPointer(m_positionAttribLocation, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+	// Tell GL how to map data from the vertex buffer "m_vbo_vertexNormals" into the
+	// "normal" vertex attribute location for any bound vertex shader program.
+	glBindBuffer(GL_ARRAY_BUFFER, m_vbo_vertexNormals);
+	glVertexAttribPointer(m_normalAttribLocation, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+	//-- Unbind target, and restore default values:
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	CHECK_GL_ERRORS;
+
+
+	//-- Unbind target, and restore default values:
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	CHECK_GL_ERRORS;
+}
+
+//----------------------------------------------------------------------------------------
+void Project::initPerspectiveMatrix()
+{
+	float aspect = ((float)m_windowWidth) / m_windowHeight;
+	m_perpsective = glm::perspective(degreesToRadians(60.0f), aspect, 0.1f, 100.0f);
+}
+
+
+//----------------------------------------------------------------------------------------
+void Project::initViewMatrix() {
+	m_view = glm::lookAt(vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 0.0f, -1.0f),
+			vec3(0.0f, 1.0f, 0.0f));
+}
+
+//----------------------------------------------------------------------------------------
+void Project::initLightSources() {
+	// World-space position
+	m_light.position = vec3(-2.0f, 5.0f, 0.5f);
+	m_light.rgbIntensity = vec3(0.8f); // White light
+}
+
+//----------------------------------------------------------------------------------------
+void Project::uploadCommonSceneUniforms() {
+	m_shader.enable();
+	{
+		//-- Set Perpsective matrix uniform for the scene:
+		GLint location = m_shader.getUniformLocation("Perspective");
+		glUniformMatrix4fv(location, 1, GL_FALSE, value_ptr(m_perpsective));
+		CHECK_GL_ERRORS;
+
+
+		//-- Set LightSource uniform for the scene:
+		{
+			location = m_shader.getUniformLocation("light.position");
+			glUniform3fv(location, 1, value_ptr(m_light.position));
+			location = m_shader.getUniformLocation("light.rgbIntensity");
+			glUniform3fv(location, 1, value_ptr(m_light.rgbIntensity));
+			CHECK_GL_ERRORS;
+		}
+
+		//-- Set background light ambient intensity
+		{
+			location = m_shader.getUniformLocation("ambientIntensity");
+			vec3 ambientIntensity(0.05f);
+			glUniform3fv(location, 1, value_ptr(ambientIntensity));
+			CHECK_GL_ERRORS;
+		}
+	}
+	m_shader.disable();
+}
+
+//----------------------------------------------------------------------------------------
+/*
+ * Called once per frame, before guiLogic().
+ */
+void Project::appLogic()
+{
+	// Place per frame, application logic here ...
+
+	uploadCommonSceneUniforms();
+}
+
+//----------------------------------------------------------------------------------------
+/*
+ * Called once per frame, after appLogic(), but before the draw() method.
+ */
+void Project::guiLogic()
+{
+	if( !show_gui ) {
+		return;
+	}
+
+	static bool firstRun(true);
+	if (firstRun) {
+		ImGui::SetNextWindowPos(ImVec2(50, 50));
+		firstRun = false;
+	}
+
+	static bool showDebugWindow(true);
+	ImGuiWindowFlags windowFlags(ImGuiWindowFlags_AlwaysAutoResize);
+	float opacity(0.5f);
+
+	ImGui::Begin("Properties", &showDebugWindow, ImVec2(100,100), opacity,
+			windowFlags);
+
+
+		// Add more gui elements here here ...
+
+
+		// Create Button, and check if it was clicked:
+		if( ImGui::Button( "Quit Application" ) ) {
+			glfwSetWindowShouldClose(m_window, GL_TRUE);
+		}
+
+		ImGui::Text( "Framerate: %.1f FPS", ImGui::GetIO().Framerate );
+
+	ImGui::End();
+}
+
+//----------------------------------------------------------------------------------------
+// Update mesh specific shader uniforms:
+static void updateShaderUniforms(
+		const ShaderProgram & shader,
+		const GeometryNode & node,
+		const glm::mat4 & viewMatrix
+) {
+
+	shader.enable();
+	{
+		//-- Set ModelView matrix:
+		GLint location = shader.getUniformLocation("ModelView");
+		mat4 modelView = viewMatrix * node.trans;
+		glUniformMatrix4fv(location, 1, GL_FALSE, value_ptr(modelView));
+		CHECK_GL_ERRORS;
+
+		//-- Set NormMatrix:
+		location = shader.getUniformLocation("NormalMatrix");
+		mat3 normalMatrix = glm::transpose(glm::inverse(mat3(modelView)));
+		glUniformMatrix3fv(location, 1, GL_FALSE, value_ptr(normalMatrix));
+		CHECK_GL_ERRORS;
+
+
+		//-- Set Material values:
+		location = shader.getUniformLocation("material.kd");
+		vec3 kd = node.material.kd;
+		glUniform3fv(location, 1, value_ptr(kd));
+		CHECK_GL_ERRORS;
+		location = shader.getUniformLocation("material.ks");
+		vec3 ks = node.material.ks;
+		glUniform3fv(location, 1, value_ptr(ks));
+		CHECK_GL_ERRORS;
+		location = shader.getUniformLocation("material.shininess");
+		glUniform1f(location, node.material.shininess);
+		CHECK_GL_ERRORS;
+
+	}
+	shader.disable();
+
+}
+
+//----------------------------------------------------------------------------------------
+/*
+ * Called once per frame, after guiLogic().
+ */
+void Project::draw() {
+
+	glEnable( GL_DEPTH_TEST );
+	renderSceneGraph(*m_rootNode);
+
+
+	glDisable( GL_DEPTH_TEST );
+}
+
+//----------------------------------------------------------------------------------------
+void Project::renderSceneGraph(const SceneNode & root) {
+
+	// Bind the VAO once here, and reuse for all GeometryNode rendering below.
+	glBindVertexArray(m_vao_meshData);
+
+	// This is emphatically *not* how you should be drawing the scene graph in
+	// your final implementation.  This is a non-hierarchical demonstration
+	// in which we assume that there is a list of GeometryNodes living directly
+	// underneath the root node, and that we can draw them in a loop.  It's
+	// just enough to demonstrate how to get geometry and materials out of
+	// a GeometryNode and onto the screen.
+
+	// You'll want to turn this into recursive code that walks over the tree.
+	// You can do that by putting a method in SceneNode, overridden in its
+	// subclasses, that renders the subtree rooted at every node.  Or you
+	// could put a set of mutually recursive functions in this class, which
+	// walk down the tree from nodes of different types.
+
+	for (const SceneNode * node : root.children) {
+
+		if (node->m_nodeType != NodeType::GeometryNode)
+			continue;
+
+		const GeometryNode * geometryNode = static_cast<const GeometryNode *>(node);
+
+		updateShaderUniforms(m_shader, *geometryNode, m_view);
+
+
+		// Get the BatchInfo corresponding to the GeometryNode's unique MeshId.
+		BatchInfo batchInfo = m_batchInfoMap[geometryNode->meshId];
+
+		//-- Now render the mesh:
+		m_shader.enable();
+		glDrawArrays(GL_TRIANGLES, batchInfo.startIndex, batchInfo.numIndices);
+		m_shader.disable();
+	}
+
+	glBindVertexArray(0);
+	CHECK_GL_ERRORS;
+}
+
+//----------------------------------------------------------------------------------------
+/*
+ * Called once, after program is signaled to terminate.
+ */
+void Project::cleanup()
+{
+
+}
+
+//----------------------------------------------------------------------------------------
+/*
+ * Event handler.  Handles cursor entering the window area events.
+ */
+bool Project::cursorEnterWindowEvent (
+		int entered
+) {
+	bool eventHandled(false);
+
+	// Fill in with event handling code...
+
+	return eventHandled;
+}
+
+//----------------------------------------------------------------------------------------
+/*
+ * Event handler.  Handles mouse cursor movement events.
+ */
+bool Project::mouseMoveEvent (
+		double xPos,
+		double yPos
+) {
+	bool eventHandled(false);
+
+	// Fill in with event handling code...
+
+	return eventHandled;
+}
+
+//----------------------------------------------------------------------------------------
+/*
+ * Event handler.  Handles mouse button events.
+ */
+bool Project::mouseButtonInputEvent (
+		int button,
+		int actions,
+		int mods
+) {
+	bool eventHandled(false);
+
+	// Fill in with event handling code...
+
+	return eventHandled;
+}
+
+//----------------------------------------------------------------------------------------
+/*
+ * Event handler.  Handles mouse scroll wheel events.
+ */
+bool Project::mouseScrollEvent (
+		double xOffSet,
+		double yOffSet
+) {
+	bool eventHandled(false);
+
+	// Fill in with event handling code...
+
+	return eventHandled;
+}
+
+//----------------------------------------------------------------------------------------
+/*
+ * Event handler.  Handles window resize events.
+ */
+bool Project::windowResizeEvent (
+		int width,
+		int height
+) {
+	bool eventHandled(false);
+	initPerspectiveMatrix();
+	return eventHandled;
+}
+
+//----------------------------------------------------------------------------------------
+/*
+ * Event handler.  Handles key input events.
+ */
+bool Project::keyInputEvent (
+		int key,
+		int action,
+		int mods
+) {
+	bool eventHandled(false);
+
+	if( action == GLFW_PRESS ) {
+		if( key == GLFW_KEY_M ) {
+			show_gui = !show_gui;
+			eventHandled = true;
+		}
+	}
+	// Fill in with event handling code...
+
+	return eventHandled;
+}
